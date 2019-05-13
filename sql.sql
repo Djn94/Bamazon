e9ff4ca8c48853972b04a19a37d842e7548ad347
Non Dewman <Dnewman1934@yahoo.com>
2019-05-13 15:08:05 -0500

    table info

--- sql.sql
+++ sql.sql
@@ -10,3 +10,16 @@ CREATE TABLE products (
   stock INTEGER (4) NOT NULL,
   PRIMARY KEY (id)
 );
+
+
+INSERT INTO products (prod_name, depart_name, cost, stock)
+VALUES ("Shoes", 'clothing', 60.00, 6), 
+("DressShirt", 'clothing', 30.00, 12), 
+("Slacks", 'clothing', 45.00, 18), 
+("DVD", 'electronics', 15.00, 205), 
+("Headphones", 'electronics', 20.00, 15), 
+("PhoneCharger", 'electronics', 5.00, 45), 
+("BluRay", 'electronics', 20.00, 113), 
+("Ribeye", 'steak', 14.00, 10), 
+("Tbone", 'steak', 19.00, 44), 
+("Filet", "steak", 64.00, 4);
