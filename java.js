0b7e130e0a6c6785650a64f8759941c17af57d08
Non Dewman <Dnewman1934@yahoo.com>
2019-05-14 18:44:52 -0500

    stickin the query in the connect

--- java.js
+++ java.js
@@ -10,59 +10,59 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
-});
-connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    let productList = res;
-    console.log(productList);
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Which item would you like to purchase? Select by ID number (1-10)',
-            name: 'selection',
-        }
-
-    ]).then(function (response, err) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        let selectionIndex = parseInt(response.selection)
-        console.log(selectionIndex)
-        let selectedItem = productList[(selectionIndex - 1)]
-        console.log(selectedItem)
-        if (selectionIndex <= 1 || selectionIndex >= 11) {
-            console.log('Invalid ID')
+        let productList = res;
+        console.log(productList);
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Which item would you like to purchase? Select by ID number (1-10)',
+                name: 'selection',
+            }
+
+        ]).then(function (response, err) {
+            if (err) throw err;
+            let selectionIndex = parseInt(response.selection)
+            console.log(selectionIndex)
+            let selectedItem = productList[(selectionIndex - 1)]
+            console.log(selectedItem)
+            if (selectionIndex <= 1 || selectionIndex >= 11) {
+                console.log('Invalid ID')
+            }
+            else {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'How many?',
+                        name: 'quantity'
+                    },
+                ]).then(function (response) {
+                    let orderAmount = response.quantity;
+                    let amountAvailable = selectedItem.stock;
+                    if (orderAmount < amountAvailable) {
+                        console.log('we can do that');
+                        let newStock = (amountAvailable - orderAmount);
+                        const query = connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    quantity: newStock
+                                },
+                            ],
+                            function (err, res) {
+                                let itemCost = selectedItem.cost;
+                                let totalCost = (itemCost * orderAmount);
+                                console.log('Total cost of your order will be ' + totalCost + ' dollars.')
+                            });
+                    }
+                    else {
+                        console.log("Sorry we don't have that many. We only have " + amountAvailable + " left.");
+                    }
+                });
+            }
         }
-        else {
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'How many?',
-                    name: 'quantity'
-                },
-            ]).then(function (response) {
-                let orderAmount = response.quantity;
-                let amountAvailable = selectedItem.stock;
-                if (orderAmount < amountAvailable) {
-                    console.log('we can do that');
-                    let newStock = (amountAvailable - orderAmount);
-                    const query = connection.query(
-                        "UPDATE products SET ? WHERE ?",
-                        [
-                            {
-                                quantity: newStock
-                            },
-                        ],
-                        function (err, res) {
-                            let itemCost = selectedItem.cost;
-                            let totalCost = (itemCost * orderAmount);
-                            console.log('Total cost of your order will be ' + totalCost + ' dollars.')
-                        });
-                }
-                else {
-                    console.log("Sorry we don't have that many. We only have " + amountAvailable + " left.");
-                }
-            });
-        }
-    }
-    );
+        );
+    });
+    connection.end();
 });
