ebed063543f20ab2d24e0dcaab29114b52416dca
Non Dewman <Dnewman1934@yahoo.com>
2019-05-18 16:32:07 -0500

    working view items and view low stock functions

--- Manager.js
+++ Manager.js
@@ -7,9 +7,26 @@ const connection = mysql.createConnection({
     password: "root",
     database: "products_db"
 });
+function selectAll() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log('aye')
+        connection.query("SELECT * FROM PRODUCTS", function (err, res) {
+            if (err) throw err;
+            console.log(res);
+
+        });
+        connection.end(); //return to menu function
+    });
+};
+function selectLoStock() {
+    connection.query("SELECT * FROM PRODUCTS WHERE stock<5", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+    });
+    connection.end(); //return to menu
+}
+
 connection.connect(function (err) {
     if (err) throw err;
-    console.log('aye')
-    connection.end();
-}
-)
+});
